529367aefda29861fea6be4ce87395d1569ad977
Jesse D. Crowe <13jcrowe@condecor.net>
2014-11-15 02:28:08 +0000

    again to be safe
    	new file:   Makefile
    	new file:   My_PRU/Makefile
    	new file:   My_PRU/My_PRU.c
    	new file:   My_PRU/My_PRU.p
    	new file:   My_PRU/obj/My_PRU.o

---
+++ My_PRU/My_PRU.c
@@ -0,0 +1,110 @@
+
+/******************************************************************************
+* Include Files                                                               *
+******************************************************************************/
+
+// Standard header files
+#include <stdio.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <string.h>
+
+// Driver header file
+#include "prussdrv.h"
+#include <pruss_intc_mapping.h>
+
+/******************************************************************************
+* Explicit External Declarations                                              *
+******************************************************************************/
+
+/******************************************************************************
+* Local Macro Declarations                                                    *
+******************************************************************************/
+
+#define PRU_NUM 	 0
+#define ADDEND1	 	 0x98765400u
+#define ADDEND2		 0x12345678u
+#define ADDEND3		 0x10210210u
+
+#define DDR_BASEADDR     0x80000000
+#define OFFSET_DDR	 0x00001000
+#define OFFSET_SHAREDRAM 2048		//equivalent with 0x00002000
+
+#define PRUSS0_SHARED_DATARAM    4
+
+/******************************************************************************
+* Local Typedef Declarations                                                  *
+******************************************************************************/
+
+/******************************************************************************
+* Local Function Declarations                                                 *
+******************************************************************************/
+
+/******************************************************************************
+* Local Variable Definitions                                                  *
+******************************************************************************/
+
+/******************************************************************************
+* Intertupt Service Routines                                                  *
+******************************************************************************/
+
+/******************************************************************************
+* Global Variable Definitions                                                 *
+******************************************************************************/
+
+static int mem_fd;
+static void *ddrMem, *sharedMem;
+
+static unsigned int *sharedMem_int;
+
+/******************************************************************************
+* Global Function Definitions                                                 *
+******************************************************************************/
+
+int main (void)
+{
+    unsigned int ret;
+    tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+
+    printf("\nINFO: Starting %s example.\r\n", "My_PRU");
+    /* Initialize the PRU */
+    prussdrv_init ();
+
+    /* Open PRU Interrupt */
+    ret = prussdrv_open(PRU_EVTOUT_0);
+    if (ret)
+    {
+        printf("prussdrv_open open failed\n");
+        return (ret);
+    }
+
+    /* Get the interrupt initialized */
+    prussdrv_pruintc_init(&pruss_intc_initdata);
+
+    /* Initialize example */
+    printf("\tINFO: Initializing example.\r\n");
+
+    /* Execute example on PRU */
+    printf("\tINFO: Executing example.\r\n");
+    prussdrv_exec_program (PRU_NUM, "./My_PRU.bin");
+
+    /* Wait until PRU0 has finished execution */
+    printf("\tINFO: Waiting for HALT command.\r\n");
+    prussdrv_pru_wait_event (PRU_EVTOUT_0);
+    printf("\tINFO: PRU completed transfer.\r\n");
+    prussdrv_pru_clear_event (PRU_EVTOUT_0, PRU0_ARM_INTERRUPT);
+
+    /* Disable PRU and close memory mapping*/
+    prussdrv_pru_disable(PRU_NUM);
+    prussdrv_exit ();
+    munmap(ddrMem, 0x0FFFFFFF);
+    close(mem_fd);
+
+    return(0);
+}
+
+/*****************************************************************************
+* Local Function Definitions                                                 *
+*****************************************************************************/
